db99ecf39e5a7bb5d384f0c7a4a7540947b69cd0
Naman Bhatia <165756218+namanbhatia-1@users.noreply.github.com>
2025-06-02 16:18:54 +0530

    Update server.js

--- myntra-clone/src/backend/server.js
+++ myntra-clone/src/backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
